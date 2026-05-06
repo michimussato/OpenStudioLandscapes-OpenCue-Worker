09be842b15914de88aa5d6013833cc80bfdcfc17
Michael Mussato <michimussato@gmail.com>
2026-05-06 15:14:08 +0200

    nox

--- src/OpenStudioLandscapes/OpenCue_Worker/config/models.py
+++ src/OpenStudioLandscapes/OpenCue_Worker/config/models.py
@@ -10,7 +10,7 @@ from pydantic import (
 )
 from pydantic_core import PydanticCustomError
 
-from OpenStudioLandscapes.OpenCue_Worker import constants, dist, LOGGER
+from OpenStudioLandscapes.OpenCue_Worker import LOGGER, constants, dist
 
 
 class Branches(enum.StrEnum):
