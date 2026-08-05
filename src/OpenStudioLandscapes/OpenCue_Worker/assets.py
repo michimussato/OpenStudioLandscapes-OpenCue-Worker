424db3bec54239fc26ed8a5447e47a9eca014e46
Michael Mussato <michimussato@gmail.com>
2026-08-05 11:39:17 +0200

    first test with rez as configurable resource

--- src/OpenStudioLandscapes/OpenCue_Worker/assets.py
+++ src/OpenStudioLandscapes/OpenCue_Worker/assets.py
@@ -27,6 +27,7 @@ from OpenStudioLandscapes.engine.common_assets import (
     group_out,
 )
 from OpenStudioLandscapes.engine.config.models import ConfigEngine
+from OpenStudioLandscapes.engine.base.configurable_resources.rez_resource import RezConfigurableResource
 from OpenStudioLandscapes.engine.constants import (
     ASSET_HEADER_BASE,
 )
@@ -248,6 +249,7 @@ def compose_networks(
 )
 def compose_rqd_worker(
     context: AssetExecutionContext,
+    config_RezConfigurableResource: RezConfigurableResource,
     CONFIG: config.models.Config,  # pylint: disable=redefined-outer-name
     CONFIG_PARENT: ConfigParent,  # pylint: disable=redefined-outer-name
     compose_networks: Dict,  # pylint: disable=redefined-outer-name
@@ -379,7 +381,7 @@ def compose_rqd_worker(
                     *_volume_relative,
                     *config_engine.global_bind_volumes,
                     *CONFIG.local_bind_volumes,
-                    *config_engine.openstudiolandscapes__rez_config.REZ_PACKAGES_PATH_VOL,
+                    *config_RezConfigurableResource.REZ_PACKAGES_PATH_VOL,
                 }
             )
         }
@@ -430,7 +432,7 @@ def compose_rqd_worker(
                         % (CONFIG.compose_scope, container_name),
                         **config_engine.global_environment_variables,
                         **CONFIG.local_environment_variables,
-                        **config_engine.openstudiolandscapes__rez_config.REZ_ENVIRONMENT,
+                        **config_RezConfigurableResource.REZ_ENVIRONMENT,
                     },
                     **copy.deepcopy(volumes_dict),
                     **copy.deepcopy(network_dict),
